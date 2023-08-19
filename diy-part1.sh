2eb0b99ce024aef1b2e8f1a70ae2aa996dfd5d33
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-19 23:06:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,4 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git tano https://github.com/tano-systems/luci-theme-tano' >>feeds.conf.default
 #echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird' >>feeds.conf.default
-echo 'src-git netgear https://github.com/Aslin-Ameng/luci-theme-netgear' >>feeds.conf.default
+#echo 'src-git netgear https://github.com/Aslin-Ameng/luci-theme-netgear' >>feeds.conf.default
