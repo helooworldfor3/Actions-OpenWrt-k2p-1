899b9d852b4d12f0515ddd64a13fd1cf0a5fa46f
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-22 19:17:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
+# 修改开源驱动wifi名称
+sed -i 's/OpenWrt/ZTE-E8820S-%s/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改闭源驱动2G wifi名称
+sed -i 's/OpenWrt_2G/ZTE-E8820S/g' package/lean/mt-drivers/mt_wifi/files/mt7603.dat
+
+# 修改闭源驱动5G wifi名称
+sed -i 's/OpenWrt_5G/ZTE-E8820S-5G/g' package/lean/mt-drivers/mt_wifi/files/mt7612.dat
