0bdb74ad79a42ef3b7a62017ee004fb32267a0fe
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-09-02 16:22:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,3 @@
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
