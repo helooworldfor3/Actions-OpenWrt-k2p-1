24e22d31fe9d15aa6c1c021d31c5d89d73e8a4a6
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-09-02 12:24:39 +0800

    Create lion.sh

---
+++ lion.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/KyleRicardo/luci-app-airwhu.git
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/BoringCat/luci-app-minieap.git package/luci-app-minieap
